782f403eb3e713914cc4d7662d9a492aef4a1d47
Sebastian de Rzewuski <Sebastian.de.Rzewuski@gmail.com>
2015-09-30 13:52:04 +0200

    Added package "org.test.just_fun", which we will use for our fun classes.

--- src/org/test/framework/TestOurTest.java
+++ src/org/test/framework/TestOurTest.java
@@ -2,8 +2,12 @@ package org.test.framework;
 
 import org.test.framework.OurTest;
 
+import org.junit.*;
+
 public class TestOurTest {
 
+	Object o;
+	
 	@OurTest
 	public void test()
 	{
@@ -11,6 +15,22 @@ public class TestOurTest {
 		//Test5
 		//Test4
 		//Test3
+	}
+	
+	@Test
+	public void t()
+	{
+		
+	}
+	
+	public boolean Assert(int expected, int actual)
+	{
+		return expected == actual;
+	}
+	
+	public void Assert(String expected, String actual)
+	{
+
 	}
 	
 }
