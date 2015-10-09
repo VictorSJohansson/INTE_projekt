788aee9975b309b9252b85f681bc39d56eeb034d
Sebastian de Rzewuski <Sebastian.de.Rzewuski@gmail.com>
2015-10-09 12:56:39 +0200

    Conflict handled
    Might've been a conflict where i changed classes directory while john
    tried to commit new directory changes

---
+++ src/org/test/framework/TestOurTest.java
@@ -0,0 +1,36 @@
+package org.test.framework;
+
+import org.test.framework.OurTest;
+
+import org.junit.*;
+
+public class TestOurTest {
+
+	Object o;
+	
+	@OurTest
+	public void test()
+	{
+		//Test
+		//Test5
+		//Test4
+		//Test3
+	}
+	
+	@Test
+	public void t()
+	{
+		
+	}
+	
+	public boolean Assert(int expected, int actual)
+	{
+		return expected == actual;
+	}
+	
+	public void Assert(String expected, String actual)
+	{
+
+	}
+	
+}
