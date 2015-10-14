e3d066c9bf192d9edbe9a1c754ca10a433571497
pinpinlarsson <pkarabatak@gmail.com>
2015-10-14 16:44:17 +0200

    char test update
    I have written the first method

--- src/org/test/framework/primitive/Test_Char.java
+++ src/org/test/framework/primitive/Test_Char.java
@@ -5,69 +5,16 @@ package src.org.test.framework.primitive;
  * Test bla bla
 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Pinar Larsson    
+ * @14/10/2015
  */
 public class Test_Char
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Test_Char
+    /* This methos will compare two char.
      */
-    public Test_Char()
+    public boolean equals_char(char a,char b )
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        
+        return a == b;
     }
 }
-=======
-package org.test.framework.primitive;
-
-
-/**
- * Write a description of class Test_Char here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Test_Char
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Test_Char
-     */
-    public Test_Char()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
->>>>>>> origin/master
