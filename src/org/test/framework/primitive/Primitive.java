51d13ea13dc25dd5f179cf75a39f8f2cf07c5f5b
VictorSJohansson <victor.s.johansson@gmail.com>
2015-10-22 11:17:23 +0100

    No more booleans
    Methods are now void instead of booleans. When the condition is not met
    the test is failed makes a LogEntry in the TestLogger. Still need to
    clean up some code and change conditions, and fail parameters.

--- src/org/test/framework/primitive/Primitive.java
+++ src/org/test/framework/primitive/Primitive.java
@@ -15,90 +15,130 @@ public class Primitive {
 	//Booleans******************************************************************************************
 
 	//compare two booleans, return true if equal
-	public static boolean equals(boolean x, boolean y){
+	public static void equals(boolean x, boolean y){
 		int a = Boolean.compare(x, y);
 		if (a==0){
-			return true;
+			return;
 		}
 		else{    
 			fail("equals", x, y);
-			return false;
+			return;
 		}
 	}
 	
 	//compare two booleans, return true if not equal
-	public static boolean notEquals(boolean x, boolean y){
+	public static void notEquals(boolean x, boolean y){
 		int a = Boolean.compare(x,y);
 		if(a!=0){
-			return true; 
+			return; 
 		}
 		else{
 			fail("notEquals", x, y);
-			return false;
+			return;
 		}
 	}
 	
 	//Byte**********************************************************************************************
 	
-	public static boolean equals(byte x, byte y) {
+	public static void equals(byte x, byte y) {
 		// TODO Auto-generated method stub
 		if (x==y){
-			return true;
+			return;
 		}
 		else{
 			fail("equals", x, y);
-			return false;
+			return;
 		}
 	}
 
-	public static boolean greater(byte x, byte y) {
+	public static void greater(byte x, byte y) {
 		// TODO Auto-generated method stub
 		if (x>y){
-			return true;
+			return;
 		}
 		else{
 			fail("greater", true, false);
-			return false;
+			return;
 		}
 	}
 	
-	public static boolean less(byte x, byte y) {
+	public static void less(byte x, byte y) {
 		// TODO Auto-generated method stub
-		return x < y;
+		if (x<y){
+			return;
+		}
+		else{
+			fail("less byte", true, false);
+			return;
+		}
 	}
 
-	public static boolean divisor(byte x, byte y) {
+	public static void divisor(byte x, byte y) {
 		// TODO Auto-generated method stub
-		return y%x == 0;
+		if (y%x == 0){
+			return;
+		}
+		else{
+			fail("divisor byte", true, false);
+			return;
+		}
 	}
 	
 	//Short*********************************************************************************************
 	
-	public static boolean equals(short x, short y) {
+	public static void equals(short x, short y) {
 		// TODO Auto-generated method stub
-		return y == x;
+		if (y == x){
+			return;
+		}
+		else{
+			fail("equals short", true, false);
+			return;
+		}
+		
 	}
 
-	public static boolean greater(short x, short y) {
+	public static void greater(short x, short y) {
 		// TODO Auto-generated method stub
-		return x > y;
+		if (x > y){
+			return;
+		}
+		else{
+			fail("greater short", true, false);
+			return;
+		}
+		
 	}
 	
-	public static boolean less(short x, short y) {
+	public static void less(short x, short y) {
 		// TODO Auto-generated method stub
-		return x < y;
+		if (x < y){
+			return;
+		}
+		else{
+			fail("less short", true, false);
+			return;
+		}
 	}
 
-	public static boolean divisor(short x, short y) {
+	public static void divisor(short x, short y) {
 		// TODO Auto-generated method stub
-		return y%x == 0;
+		if (y%x == 0){
+			return;
+		}
+		else{
+			fail("divisor short", true, false);
+			return;
+		}
 	}
 
-	public static boolean valueWithinByteRange(short x) {
+	public static void valueWithinByteRange(short x) {
 		// TODO Auto-generated method stub
-		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE)
-			return false;
-		return true;
+		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE){
+			fail("valueWithinByteRange", true, false);
+			return;
+		}
+		return;
 	}
 	
 	//Int***********************************************************************************************
@@ -106,9 +146,15 @@ public class Primitive {
     /**
     * Following method tests if given integers x and y are equal to each other.
     */
-   public static boolean equals(int x, int y)
+   public static void equals(int x, int y)
    {  
-       return x == y;
+       if (x == y){
+			return;
+		}
+		else{
+			fail("equals int", true, false);
+			return;
+		}
    
    }
     
@@ -116,37 +162,57 @@ public class Primitive {
    /**
     * Following method tests if, for given integers x and y, it holds that x is strictly greater than y.
     */
-   public static boolean greater(int x, int y)
+   public static void greater(int x, int y)
    {
-       return x > y;
+       if (x > y){
+			return;
+		}
+		else{
+			fail("greater int", true, false);
+			return;
+		}
    }
    
     /**
     * Following method tests if, for given integers x and y, it holds that x is strictly less than y.
     */
-   public static boolean less(int x, int y)
+   public static void less(int x, int y)
    {
-       return x < y;
+       if (x < y){
+			return;
+		}
+		else{
+			fail("less int", true, false);
+			return;
+		}
    }
    
     /**
     * Following method tests if, for given integers x and y, it holds that x is a divisor of y.
     */
-   public static boolean divisor(int x, int y)
+   public static void divisor(int x, int y)
    {
-       return y%x == 0;
+       if (y%x == 0){
+			return;
+		}
+		else{
+			fail("divisor int", true, false);
+			return;
+		}
    }
    
-   public static boolean valueWithinShortRange(int x) { //added by John Thorelli 20/10/2015
-		if(x < Short.MIN_VALUE || x > Short.MAX_VALUE)
-			return false;
-		return true;
+   public static void valueWithinShortRange(int x) { //added by John Thorelli 20/10/2015
+		if(x < Short.MIN_VALUE || x > Short.MAX_VALUE){
+			fail("valueWithinShortRange", true, false);
+		}
+		return;
    }
    
-	public static boolean valueWithinByteRange(int x) { //added by John Thorelli 20/10/2015
-		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE)
-			return false;
-		return true;
+	public static void valueWithinByteRange(int x) { //added by John Thorelli 20/10/2015
+		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE){
+			fail("valueWithinByteRange", true, false);
+		}
+		return;
 	}
 	
 	//Long**********************************************************************************************
@@ -155,86 +221,125 @@ public class Primitive {
 	 * This method checks to see if long x and long y holds the same value
 	 * and returns true if they do, false otherwise.
 	 */
-	public static boolean equals(long x, long y){
+	public static void equals(long x, long y){
 		
 		int a = Long.compare(x, y);
 		if(a==1){
-			return true;
+			return;
 		}
-		else 
-			return false;
+		else {
+			fail("equals long", true, false);
+			return;
+		}
 	}
 	
 	/*
 	 * This method returns true if long x is greater than long y.
 	 */
-	public static boolean greater(long x, long y){
+	public static void greater(long x, long y){
 		
 		int a = Long.compare(x, y);
 		if(a>0){
-			return true;
+			return;
 		}
-		else 
-			return false;
+		else {
+			fail("long greater", true, false);
+			return;
+			}
 	}
 	
 	/*
 	 * Compares two longs and returns true if x is less than y.
 	 */
-	public static boolean less(long x, long y){
+	public static void less(long x, long y){
 		
 		int a = Long.compare(x, y);
 		if(a<0){
-			return true;
+			return;
 		}
-		else 
-			return false;
+		else {
+			fail("long less", true, false);
+			return;
+		}
 	}
 	
-    public static boolean divisor(long x, long y) { //added by John Thorelli 20/10/2015
-        return y%x == 0;
+    public static void divisor(long x, long y) { //added by John Thorelli 20/10/2015
+    	if (y%x == 0){
+			return;
+		}
+		else{
+			fail("divisor long", true, false);
+			return;
+		}
     }
 
-	public static boolean valueWithinIntRange(long x) { //added by John Thorelli 20/10/2015
-		if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE)
-			return false;
-		return true;
+	public static void valueWithinIntRange(long x) { //added by John Thorelli 20/10/2015
+		if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE){
+			fail("valueWithinIntRange long", true, false);
+		}
+		return;
 	}
 
-	public static boolean valueWithinShortRange(long x) { //added by John Thorelli 20/10/2015
-		if(x < Short.MIN_VALUE || x > Short.MAX_VALUE)
-			return false;
-		return true;
+	public static void valueWithinShortRange(long x) { //added by John Thorelli 20/10/2015
+		if(x < Short.MIN_VALUE || x > Short.MAX_VALUE){
+			fail("valueWithinShortRange long", true, false);
+		}
+		return;
 	}
 
-	public static boolean valueWithinByteRange(long x) { //added by John Thorelli 20/10/2015
-		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE)
-			return false;
-		return true;
+	public static void valueWithinByteRange(long x) { //added by John Thorelli 20/10/2015
+		if(x < Byte.MIN_VALUE || x > Byte.MAX_VALUE){
+			fail("valueWithinByteRange long", true, false);
+		}
+		return;
 	}
 	
 	//Float*********************************************************************************************
 	
-	public static boolean equals(short x, float y) {
+	public static void equals(short x, float y) {
 		// TODO Auto-generated method stub
-		return y == x;
+		if(y == x){
+			return;
+		}
+		else {
+			fail("equals short", true, false);
+			return;
+		}
 	}
 
-	public static boolean greater(float x, float y) {
+	public static void greater(float x, float y) {
 		// TODO Auto-generated method stub
-		return x > y;
+		if(x > y){
+			return;
+		}
+		else {
+			fail("greater short", true, false);
+			return;
+		}
 	}
 	
-	public static boolean less(float x, float y) {
+	public static void less(float x, float y) {
 		// TODO Auto-generated method stub
-		return x < y;
+		if(x < y){
+			return;
+		}
+		else {
+			fail("less float", true, false);
+			return;
+		}
 	}
 
-	public static boolean sameIntegerValue(float x, float y) {
+	public static void sameIntegerValue(float x, float y) {
 		// TODO Auto-generated method stub
         int a = (int) x;
         int b = (int) y;
-        return a == b;
+		if(a==b){
+			return;
+		}
+		else {
+			fail("sameIntegerValue float", true, false);
+			return;
+		}
     }
 	
 	//Doubble*******************************************************************************************
@@ -242,62 +347,80 @@ public class Primitive {
     /**
    * Following method tests if given doubles x and y are equal to each other.
    */
-  public static boolean equals(double x, double y)
+  public static void equals(double x, double y)
   {  
       int a = Double.compare(x, y);
       if(a == 0)
-      return true;
-      else
-      return false;
+      return;
+      else{
+    	  fail("equals double", true, false);
+    	  return;
+      }
   }
    
   /**
    * Following method tests if, for given doubles x and y, it holds that x is greater than y.
    */
-  public static boolean greater(double x, double y)
+  public static void greater(double x, double y)
   {
       int a = Double.compare(x, y);
       if(a > 0)
-      return true;
-      else
-      return false;
+      return;
+      else{
+    	  fail("greater double", true, false);
+    	  return;
+      }
   }
   
    /**
    * Following method tests if, for given doubles x and y, it holds that x is less than y.
    */
-  public static boolean less(double x, double y)
+  public static void less(double x, double y)
   {
       int a = Double.compare(x, y);
       if(a < 0)
-      return true;
-      else
-      return false;
+      return;
+      else{
+    	  fail("less double", true, false);
+	      return;
+	   }
   }
 
    /**
    * Following method tests if, for given doubles x and y, they have the same integer value.
    */
-  public static boolean equalIntegerValue(double x, double y)
+  public static void equalIntegerValue(double x, double y)
   {
       int a = (int) x;
       int b = (int) y;
-      return a == b;
+      if(a == b){
+    	  return;
+      }
+      else{
+    	  fail("equalIntegerValue double", true, false);
+    	  return;
+      }
   }
 
-	public static boolean valueWithinFloatRange(double x) { //added by John Thorelli 20/10/2015
-		if(x < Float.MIN_VALUE || x > Float.MAX_VALUE)
-			return false;
-		return true;
+	public static void valueWithinFloatRange(double x) { //added by John Thorelli 20/10/2015
+		if(x < Float.MIN_VALUE || x > Float.MAX_VALUE){
+			fail("valueWithinFloatRange double", true, false);
+		}
+		return;
 	} 
 	
 	//Char**********************************************************************************************
 	
     /* This method will compare two char.
      */
-    public static boolean equals(char a, char b )
+    public static void equals(char a, char b )
     {        
-        return a == b;
+        if(a==b){
+        	return;
+        }
+        else{
+        	fail("equals char", true, false);
+        }
     }
     
     //Fail**********************************************************************************************
