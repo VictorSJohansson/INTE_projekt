b96e2291f50537621431641e5a27ce009bbb7867
VictorSJohansson <victor.s.johansson@gmail.com>
2015-10-24 20:19:10 +0100

    Started breaking out LogEntry
    LogEntry will be superclass for types of entries

--- src/org/test/framework/core/runtime/LogEntry.java
+++ src/org/test/framework/core/runtime/LogEntry.java
@@ -1,63 +1,23 @@
 package org.test.framework.core.runtime;
 
-import java.lang.reflect.Field;
-
-public class LogEntry {
+public abstract class LogEntry {
 	
 	/*
 	 * This class is used to store information about failed tests
 	 */
 	
 	private String testName;
-	private Object value1;
-	private Object value2;
 	
-	//Constructor when comparing two values
-	public LogEntry(String testName, Object value1, Object value2){
+	
+	//Constructor
+	public LogEntry(String testName){
 		this.testName = testName;
-		this.value1 = value1;
-		this.value2 = value2;
+		
+		//Skicka in nullvärde istället
 	}
-	
-	//Constructor when comparing one value
-	public LogEntry(String testName, Object value){
-		this.testName = testName;
-		this.value1 = value;
-	}
-	
+
 	//Returns name of test
 	public String getTestName(){
 		return testName;
 	}
-	
-	//Returns the first value submitted
-	public Object getValue1(){
-		return value1;
-	}
-	
-	//Returns the second value
-	public Object getValue2(){
-		return value2;
-	}
-	
-	/*
-	 * The following method returns a string with the value of the expected object, weather it be
-	 * of any primitve datatypes.
-	 */
-	public String getValue(Object entryObject){
-		try{
-			for (Field field : entryObject.getClass().getDeclaredFields()){
-		
-				field.setAccessible(true);
-				Object value = field.get(entryObject);
-				if (value != null){
-					return value.toString();
-				}
-			}
-			}catch(final IllegalAccessException e){
-				e.printStackTrace();
-			}
-		return "failed to find a value";
-	}
-
 }
